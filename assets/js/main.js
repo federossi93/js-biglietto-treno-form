f6e112783be7055138c16b6badd95241ba5458ad
federossi93 <rossifederico0993@gmail.com>
2022-10-07 20:24:26 +0200

    add allert

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,39 +8,60 @@
 
 let nome = document.getElementById('nome')
 
-let button = document.getElementById('genera')
+let genera = document.getElementById('genera')
+
+let annulla = document.getElementById('delete')
 
 let anni = document.getElementById('anni')
 
 let km = document.getElementById('km')
 
 let prezzo = 0.21
+//return
+genera.addEventListener('click' , function(){
+        if(document.getElementById('nome_passeggero').innerHTML =(nome.value) == ''||
+                anni.value == ''||
+                km.value == '' 
+        ) {
+                alert('Compila tutti i campi')
+                return
+}
 
-button.addEventListener('click' , function(){
+        let biglietto = km.value * prezzo 
 
-    let biglietto = km.value * prezzo 
+        genera = document.getElementById('wiew').style.display = 'block'
 
-    let nomePasseggero = document.getElementById('nome_passeggero').innerHTML =(nome.value)
-    //console.log(nome.value);
-    //console.log(km.value);
-    //console.log(anni.value);
-    document.getElementById('costo_biglietto').innerHTML =(biglietto).toFixed(2) + (' €')
-    //console.log(biglietto);
-    document.getElementById('random').innerHTML = Math.floor((Math.random() * 100000) + 1)
+        document.getElementById('nome_passeggero').innerHTML =(nome.value)
+        //console.log(nome.value);
+         //console.log(km.value);
+        //console.log(anni.value);
 
-    document.getElementById('carrozza').innerHTML = Math.floor((Math.random() * 10) + 1)
+        document.getElementById('costo_biglietto').innerHTML =(biglietto).toFixed(2) + (' €')
+
+        //console.log(biglietto);
+        document.getElementById('random').innerHTML = Math.floor((Math.random() * 100000) + 1)
+
+        document.getElementById('carrozza').innerHTML = Math.floor((Math.random() * 10) + 1)
 
 
-    if (anni.value < 18){
-        let discount = (biglietto - biglietto * 0.2)
-        let scontoMinore = document.getElementById('sconto').innerHTML =('Sconto Minorenne')
-        console.log(discount.toFixed(2))
+        if (anni.value < 18){
+                let discount = (biglietto - biglietto * 0.2)
 
-} else if (anni.value > 65){
-        let discount = (biglietto - biglietto * 0.4)
-        let scontoAnziano = document.getElementById('sconto').innerHTML =('Sconto Anziano')
-        console.log(discount.toFixed(2))
-} 
+                document.getElementById('sconto').innerHTML =('Sconto Minorenne')
+
+                console.log(discount.toFixed(2))
+
+        } else if (anni.value > 65){
+                let discount = (biglietto - biglietto * 0.4)
+                document.getElementById('sconto').innerHTML =('Sconto Anziano')
+                console.log(discount.toFixed(2))
+        } else {
+                document.getElementById('sconto').innerHTML =('Sconto non disponibile')
+        }
+})
+
+annulla.addEventListener('click' , function(){
+        annulla = document.getElementById('wiew').style.display = 'none'
 })
 
 
